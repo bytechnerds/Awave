3071dc56ac66b1d93a29d7dc966b1aebd34fb381
Matheus <matheus.modanez@gmail.com>
2021-08-23 11:16:42 -0300

    implementation of focus mode(test)

--- src/frontend/parsingJson.js
+++ src/frontend/parsingJson.js
@@ -1,21 +1,25 @@
 var teste = document.getElementById("test");
 
-function navigateButtons(){
-   
-    var items = [];
-    items.push(document.getElementsByTagName('button'))
-    console.log(items)
-    
-    for (let index = 0; index < items.length; index++) {
-        //ouvir a tecla pressionada
-        window.addEventListener("keydown", function(e) {
-            
-            if (e.key === "b") {
-               button = document.getElementById(items[index])
-               button.focus()
-            }
-        console.log(e)
-        })
-    }
-}
+function navigateButtons() {
 
+    var items = ['button1', 'button2', 'button3', 'button4'];
+    // items.push(document.getElementsByTagName('button'))
+    console.log(items)
+
+    //ouvir a tecla pressionada
+    window.addEventListener("keydown", function (e) {
+
+        console.log(e)
+        let index = 0;
+
+        if (e.key === "b") {
+            console.log(index)
+            
+            button = document.getElementById(items[index])
+            button.style.border = '2px solid red'
+            button.focus()
+
+        }
+
+    })
+}
